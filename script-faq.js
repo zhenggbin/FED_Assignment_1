b607721aeff48d6443441dddf89941ddc6c98353
zhenggbin <S10257745@connect.np.edu.sg>
2023-12-03 20:49:47 +0800

    Upload FAQ Page

---
+++ script-faq.js
@@ -0,0 +1,59 @@
+// Show or hide the answers to the question
+var acc = document.getElementsByClassName("accordion");
+for (var i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    this.parentElement.classList.toggle("active");
+
+    var pannel = this.nextElementSibling;
+
+    if (pannel.style.display === "block") {
+      pannel.style.display = "none";
+    } else {
+      pannel.style.display = "block";
+    }
+
+    // Rotate the Chevron Icon when information is showed
+    var chevron = this.querySelector("i.fa");
+    chevron.classList.toggle("rotate");
+  });
+}
+
+// Smooth scrolling to top function 
+function scrollingTOP() {
+  window.scrollTo({
+      behavior: "smooth",
+      top: 0
+  });
+}
+
+// Show or hide the scroll to top button based on scroll position
+document.addEventListener("scroll", function () {
+  const scrollToTopBtn = document.getElementById("back-to-top-btn");
+  if (scrollToTopBtn) {
+      // Using if-else loop to check whether the scrolling position is more than 100px from the top of the page
+      if (document.documentElement.scrollTop > 100) {
+          scrollToTopBtn.style.display = "block"; // if true, display the button
+      } else {
+          scrollToTopBtn.style.display = "none"; // if false, hide the button
+      }
+  }
+});
+
+// Scrolling Animation (As user scroll down, each sections of the page start to pop out)
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.scrolling-animation').forEach(section => {
+    const obs = new IntersectionObserver(entries => {
+      // Check whether there is intersection when entry when scrolling down
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show'); //if true, display the content.
+        }
+      });
+    }, { threshold: 0.2 }); 
+
+    obs.observe(section);
+  });
+});
+  
+
